fcd0aab245256a84ccb8c477ebe28c990d713b79
nicoladimaio <49908808+nicoladimaio@users.noreply.github.com>
2026-06-25 14:27:26 +0200

    Fixato errore favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,12 +57,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/assets/logo1_tab_premium.svg", type: "image/svg+xml" },
+      { url: "/icon.png", sizes: "512x512", type: "image/png" },
     ],
-    shortcut: ["/favicon-32x32.png"],
+    shortcut: ["/favicon.ico"],
     apple: [{ url: "/apple-icon.png", sizes: "512x512", type: "image/png" }],
   },
   other: {
